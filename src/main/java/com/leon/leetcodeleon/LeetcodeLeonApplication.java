ee4afd746d7ebdaa53f29492611373d9bdd8a5f0
张云飞 <zhangsmile90@gmail.com>
2019-09-22 16:45:06 +0800

    first comit

---
+++ src/main/java/com/leon/leetcodeleon/LeetcodeLeonApplication.java
@@ -0,0 +1,13 @@
+package com.leon.leetcodeleon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeetcodeLeonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeetcodeLeonApplication.class, args);
+	}
+
+}
