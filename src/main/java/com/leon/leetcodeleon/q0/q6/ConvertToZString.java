034b77c615411fd9cff85f367260f2b5d2cb274e
张云飞 <zhangsmile90@gmail.com>
2019-10-13 10:50:52 +0800

    简化Z字转换

--- src/main/java/com/leon/leetcodeleon/q0/q6/ConvertToZString.java
+++ src/main/java/com/leon/leetcodeleon/q0/q6/ConvertToZString.java
@@ -11,41 +11,45 @@ public class ConvertToZString
 	@Test
 	public void test()
 	{
-		//		assert convert(str1, 4).equalsIgnoreCase(res4);
-		//		assert convert(str1, 3).equalsIgnoreCase(res3);
+		assert convert(str1, 4).equalsIgnoreCase(res4);
+		assert convert(str1, 3).equalsIgnoreCase(res3);
 		assert convert("AB", 1).equalsIgnoreCase("AB");
 	}
 
 	public String convert(String s, int numRows)
 	{
-		if (s == null || s.length() <= 1)
+		if (s == null || s.length() <= 1 || numRows <= 1)
 		{
 			return s;
 		}
 		StringBuilder res = new StringBuilder();
-		int l = numRows > 1 ? (numRows - 1) * 2 : 1;
-		for (int i = 0; i < s.length(); i += l)
+		int l = (numRows - 1) * 2;
+		//		for (int i = 0; i < s.length(); i += l)
+		//		{
+		//			res.append(s.charAt(i));
+		//		}
+		for (int k = 0; k < numRows; k++)
 		{
-			res.append(s.charAt(i));
-		}
-		for (int k = 1; k < numRows - 1; k++)
-		{
+			int per = -1;
 			for (int i = 0; i - k < s.length(); i += l)
 			{
-				if (i - k > 0)
+				if (i - k > 0 && i - k != per)
 				{
 					res.append(s.charAt(i - k));
+					per = i - k;
 				}
-				if (i + k < s.length())
+				if (i + k < s.length() && i + k != per)
 				{
 					res.append(s.charAt(i + k));
+					per = i + k;
 				}
 			}
 		}
-		for (int i = numRows - 1; numRows > 1 && i < s.length(); i += l)
-		{
-			res.append(s.charAt(i));
-		}
+//		for (int i = numRows - 1; numRows > 1 && i < s.length(); i += l)
+//		{
+//			res.append(s.charAt(i));
+//		}
+		System.out.println(res);
 		return res.toString();
 	}
 
