d424fb5d4abb5936cb69bd69888ede13779673f9
张云飞 <zhangsmile90@gmail.com>
2019-10-27 11:53:15 +0800

    q031 框架

---
+++ src/main/java/com/leon/leetcodeleon/q000/q031/Q031NextPuermutation.java
@@ -0,0 +1,20 @@
+package com.leon.leetcodeleon.q000.q031;
+
+import com.leon.leetcodeleon.common.BaseTest;
+import org.junit.Test;
+
+public class Q031NextPuermutation extends BaseTest
+{
+	@Test
+	public void test()
+	{
+		nextPermutation(new int[] { 1, 2, 3 });
+		nextPermutation(new int[] { 3, 2, 1 });
+		nextPermutation(new int[] { 1, 5, 2 });
+	}
+
+	public void nextPermutation(int[] nums)
+	{
+
+	}
+}
