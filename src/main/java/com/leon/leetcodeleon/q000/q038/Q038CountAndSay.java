e4649e42991e1983c4375cf794f6735619735e1f
张云飞 <zhangsmile90@gmail.com>
2019-11-01 07:11:57 +0800

    优化报数

--- src/main/java/com/leon/leetcodeleon/q000/q038/Q038CountAndSay.java
+++ src/main/java/com/leon/leetcodeleon/q000/q038/Q038CountAndSay.java
@@ -13,12 +13,55 @@ public class Q038CountAndSay extends BaseTest
 	{
 		//		assert countAndSay(1).equalsIgnoreCase("1");
 		//		assert countAndSay(2).equalsIgnoreCase("11");
-//		assert countAndSay(3).equalsIgnoreCase("21");
+		//		assert countAndSay(3).equalsIgnoreCase("21");
 		assert countAndSay(4).equalsIgnoreCase("1211");
 
 	}
 
 	public String countAndSay(int n)
+	{
+		List<Integer> list = new ArrayList<>();
+		List<Integer> per = new ArrayList<>();
+		list.add(1);
+		per = list;
+		for (int i = 2; i <= n; i++)
+		{
+			list = new ArrayList<>();
+			int left = 0;
+			int right = 0;
+			while (left < per.size())
+			{
+				if (right >= per.size())
+				{
+					list.add(right - left);
+					list.add(per.get(left));
+					break;
+				}
+				else if (per.get(right) != per.get(left))
+				{
+					list.add(right - left);
+					list.add(per.get(left));
+					left = right;
+					right++;
+				}
+				else
+				{
+					right++;
+				}
+
+			}
+			per = list;
+		}
+		StringBuilder res = new StringBuilder();
+		for (Integer character : list)
+		{
+			res.append((char)('0' + character));
+		}
+		System.out.println(res.toString());
+		return res.toString();
+	}
+
+	public String countAndSayBak(int n)
 	{
 		List<Character> list = new ArrayList<>();
 		List<Character> per = new ArrayList<>();
