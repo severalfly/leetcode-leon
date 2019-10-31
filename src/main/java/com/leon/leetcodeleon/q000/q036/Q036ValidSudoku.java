c19b7f4b8e1c3e30e962a5caba8d602ef82109fa
张云飞 <zhangsmile90@gmail.com>
2019-10-31 21:41:46 +0800

    修复数独

--- src/main/java/com/leon/leetcodeleon/q000/q036/Q036ValidSudoku.java
+++ src/main/java/com/leon/leetcodeleon/q000/q036/Q036ValidSudoku.java
@@ -28,16 +28,41 @@ public class Q036ValidSudoku
 
 	public boolean isValidSudoku(char[][] board)
 	{
-		return false;
+		boolean[][] row = new boolean[9][9];
+		boolean[][] column = new boolean[9][9];
+		boolean[][] block = new boolean[9][9];
+
+		for (int i = 0; i < board.length; i++)
+		{
+			for (int j = 0; j < board[0].length; j++)
+			{
+				if (board[i][j] != '.')
+				{
+					int num = board[i][j] - '1';
+					int blockIndex = i / 3 * 3 + j / 3;
+					if (row[i][num] || column[j][num] || block[blockIndex][num])
+					{
+						return false;
+					}
+					else
+					{
+						row[i][num] = true;
+						column[j][num] = true;
+						block[blockIndex][num] = true;
+					}
+				}
+			}
+		}
+		return true;
 	}
 
-	private void print(char[][] b1)
+	private void print(char[][] board)
 	{
-		for (int i = 0; i < b1.length; i++)
+		for (int i = 0; i < board.length; i++)
 		{
-			for (int j = 0; j < b1[0].length; j++)
+			for (int j = 0; j < board[0].length; j++)
 			{
-				System.out.print(b1[i][j]);
+				System.out.print(board[i][j]);
 				System.out.print(' ');
 			}
 			System.out.println();
