0a44b30873f842fe7573ce6c4e3c4d24cf997be2
张云飞 <zhangsmile90@gmail.com>
2019-11-24 10:15:44 +0800

    占坑 45

---
+++ src/main/java/com/leon/leetcodeleon/q000/q045/Q045JumpSecondVersion.java
@@ -0,0 +1,18 @@
+package com.leon.leetcodeleon.q000.q045;
+
+import com.leon.leetcodeleon.common.BaseTest;
+import org.junit.Test;
+
+public class Q045JumpSecondVersion extends BaseTest
+{
+	@Test
+	public void test()
+	{
+		assert jump(new int[] { 2, 3, 1, 1, 4 }) == 2;
+	}
+
+	public int jump(int[] nums)
+	{
+		return 0;
+	}
+}
