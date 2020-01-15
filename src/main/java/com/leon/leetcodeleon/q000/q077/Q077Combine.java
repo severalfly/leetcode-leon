c779aba099ce486973a5d2884214a6125e272ae2
张云飞 <zhangsmile90@gmail.com>
2020-01-15 10:39:39 +0800

    占坑077

---
+++ src/main/java/com/leon/leetcodeleon/q000/q077/Q077Combine.java
@@ -0,0 +1,21 @@
+package com.leon.leetcodeleon.q000.q077;
+
+import com.alibaba.fastjson.JSONObject;
+import org.junit.Test;
+
+import java.util.List;
+
+public class Q077Combine
+{
+	@Test
+	public void testCombine()
+	{
+		List<List<Integer>> combine = combine(4, 2);
+		System.out.println(JSONObject.toJSONString(combine));
+	}
+
+	public List<List<Integer>> combine(int n, int k)
+	{
+		return null;
+	}
+}
