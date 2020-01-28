b835603cd489e0175bcd32338ea8ec5be3ecad6e
张云飞 <zhangsmile90@gmail.com>
2020-01-28 20:51:05 +0800

    补充非递归的中序遍历

--- src/main/java/com/leon/leetcodeleon/q000/q094/Q094InorderTraversal.java
+++ src/main/java/com/leon/leetcodeleon/q000/q094/Q094InorderTraversal.java
@@ -2,17 +2,39 @@ package com.leon.leetcodeleon.q000.q094;
 
 import com.leon.leetcodeleon.common.BaseTest;
 import com.leon.leetcodeleon.common.TreeNode;
+import com.leon.leetcodeleon.util.ObjectUtils;
+import com.leon.leetcodeleon.util.TreeNodeUtils;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class Q094InorderTraversal extends BaseTest
 {
 	@Test
 	public void test()
 	{
+		ObjectUtils.print(inorderTraversalNoneRepeat(TreeNodeUtils.generateTreeV2(new Integer[] { 1, null, 2, 3 })));
+	}
 
+	public List<Integer> inorderTraversalNoneRepeat(TreeNode root)
+	{
+		List<Integer> res = new ArrayList<>();
+		Stack<TreeNode> stack = new Stack<>();
+		TreeNode cur = root;
+		while (cur != null || stack.size() > 0)
+		{
+			while (cur != null)
+			{
+				stack.push(cur);
+				cur = cur.left;
+			}
+			cur = stack.pop();
+			res.add(cur.val);
+			cur = cur.right;
+		}
+		return res;
 	}
 
 	public List<Integer> inorderTraversal(TreeNode root)
--- src/main/java/com/leon/leetcodeleon/util/TreeNodeUtils.java
+++ src/main/java/com/leon/leetcodeleon/util/TreeNodeUtils.java
@@ -4,10 +4,65 @@ import com.leon.leetcodeleon.common.TreeNode;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class TreeNodeUtils
 {
+	public static TreeNode generateTreeV2(Integer[] nums)
+	{
+
+		if (nums.length == 0)
+			return new TreeNode(0);
+		Queue<TreeNode> nodeQueue = new LinkedList<>();
+		// 创建一个根节点
+		TreeNode root = new TreeNode(nums[0]);
+		nodeQueue.offer(root);
+		TreeNode cur;
+		// 记录当前行节点的数量（注意不一定是2的幂，而是上一行中非空节点的数量乘2）
+		int lineNodeNum = 2;
+		// 记录当前行中数字在数组中的开始位置
+		int startIndex = 1;
+		// 记录数组中剩余的元素的数量
+		int restLength = nums.length - 1;
+
+		while (restLength > 0)
+		{
+			// 只有最后一行可以不满，其余行必须是满的
+			//            // 若输入的数组的数量是错误的，直接跳出程序
+			//            if (restLength < lineNodeNum) {
+			//                System.out.println("Wrong Input!");
+			//                return new TreeNode(0);
+			//            }
+			for (int i = startIndex; i < startIndex + lineNodeNum; i = i + 2)
+			{
+				// 说明已经将nums中的数字用完，此时应停止遍历，并可以直接返回root
+				if (i == nums.length)
+					return root;
+				cur = nodeQueue.poll();
+				if (nums[i] != null)
+				{
+					cur.left = new TreeNode(nums[i]);
+					nodeQueue.offer(cur.left);
+				}
+				// 同上，说明已经将nums中的数字用完，此时应停止遍历，并可以直接返回root
+				if (i + 1 == nums.length)
+					return root;
+				if (nums[i + 1] != null)
+				{
+					cur.right = new TreeNode(nums[i + 1]);
+					nodeQueue.offer(cur.right);
+				}
+			}
+			startIndex += lineNodeNum;
+			restLength -= lineNodeNum;
+			lineNodeNum = nodeQueue.size() * 2;
+		}
+
+		return root;
+	}
+
 	public static TreeNode generateTree(Integer[] array, int index)
 	{
 		if (array == null || array.length <= 0)
@@ -56,6 +111,8 @@ public class TreeNodeUtils
 	public void test()
 	{
 		// printInOrder(generateTree(new Integer[] { 1, null, 2, 3 }, 0));
-		printInOrder(generateTree(new Integer[] { 1, 2, 3, 4, 5, 6 }, 0));
+		// printInOrder(generateTree(new Integer[] { 1, 2, 3, 4 }, 0));
+		// printInOrder(generateTree(new Integer[] { 1, 2, 3, 4, 5 }, 0));
+		printInOrder(generateTreeV2(new Integer[] { 5, 4, 8, 11, null, 13, 4, 7, 2, null, null, null, 1 }));
 	}
 }
