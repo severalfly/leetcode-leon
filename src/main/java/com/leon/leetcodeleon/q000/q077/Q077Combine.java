60ddea368f65e3940237a5431de6b20c548f74dc
张云飞 <zhangsmile90@gmail.com>
2020-02-04 14:39:08 +0800

    解决077

--- src/main/java/com/leon/leetcodeleon/q000/q077/Q077Combine.java
+++ src/main/java/com/leon/leetcodeleon/q000/q077/Q077Combine.java
@@ -1,8 +1,11 @@
 package com.leon.leetcodeleon.q000.q077;
 
 import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.serializer.SerializerFeature;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Q077Combine
@@ -11,11 +14,32 @@ public class Q077Combine
 	public void testCombine()
 	{
 		List<List<Integer>> combine = combine(4, 2);
-		System.out.println(JSONObject.toJSONString(combine));
+		System.out.println(JSONObject.toJSONString(combine, SerializerFeature.DisableCircularReferenceDetect));
 	}
+
+	private List<List<Integer>> list = new ArrayList<>();
 
 	public List<List<Integer>> combine(int n, int k)
 	{
-		return null;
+		// aaa
+		LinkedList<Integer> cur = new LinkedList<>();
+		backtrack(1, cur, n, k);
+		return list;
 	}
+
+	private void backtrack(int first, LinkedList<Integer> cur, int n, int k)
+	{
+		if (cur.size() == k)
+		{
+			List<Integer> tmp = new ArrayList<>(cur);
+			list.add(tmp);
+		}
+		for (int i = first; i <= n; i++)
+		{
+			cur.offer(i);
+			backtrack(i + 1, cur, n, k);
+			cur.pollLast();
+		}
+	}
+
 }
