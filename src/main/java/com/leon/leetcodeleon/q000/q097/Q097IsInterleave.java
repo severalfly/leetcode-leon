b1a7c81ce30127f464e0a9b6da5a83e44fe7ffea
张云飞 <zhangsmile90@gmail.com>
2020-03-12 19:23:05 +0800

    优化097

--- src/main/java/com/leon/leetcodeleon/q000/q097/Q097IsInterleave.java
+++ src/main/java/com/leon/leetcodeleon/q000/q097/Q097IsInterleave.java
@@ -9,9 +9,41 @@ public class Q097IsInterleave extends BaseTest
 	@Test
 	public void test()
 	{
-		assert isInterleave("abc", "df", "abdfc");
-		// assert isInterleave("aabcc", "dbbca", "aadbbcbcac");
-		// assert !isInterleave("aabcc", "dbbca", "aadbbbaccc");
+		assert isInterleaveV2("abc", "df", "abdfc");
+		assert isInterleaveV2("aabcc", "dbbca", "aadbbcbcac");
+		assert !isInterleaveV2("aabcc", "dbbca", "aadbbbaccc");
+	}
+
+	public boolean isInterleaveV2(String s1, String s2, String s3)
+	{
+		if (s3.length() != s2.length() + s1.length())
+		{
+			return false;
+		}
+		boolean dp[] = new boolean[s2.length() + 1];
+		for (int i = 0; i <= s1.length(); i++)
+		{
+			for (int j = 0; j <= s2.length(); j++)
+			{
+				if (i == 0 && j == 0)
+				{
+					dp[j] = true;
+				}
+				else if (i == 0)
+				{
+					dp[j] = dp[j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1);
+				}
+				else if (j == 0)
+				{
+					dp[j] = dp[j] && s1.charAt(i - 1) == s3.charAt(i + j - 1);
+				}
+				else
+				{
+					dp[j] = (dp[j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1)) || (dp[j] && s1.charAt(i - 1) == s3.charAt(i + j - 1));
+				}
+			}
+		}
+		return dp[s2.length()];
 	}
 
 	public boolean isInterleave(String s1, String s2, String s3)
