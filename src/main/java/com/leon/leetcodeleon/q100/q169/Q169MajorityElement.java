6690947bfe64c6f7e8f493129573264cd5cd443a
张云飞 <zhangsmile90@gmail.com>
2020-03-13 09:49:22 +0800

    优化169

--- src/main/java/com/leon/leetcodeleon/q100/q169/Q169MajorityElement.java
+++ src/main/java/com/leon/leetcodeleon/q100/q169/Q169MajorityElement.java
@@ -10,8 +10,8 @@ public class Q169MajorityElement
 	@Test
 	public void test()
 	{
-		assert majorityElement(new int[] { 3, 2, 3 }) == 3;
-		assert majorityElement(new int[] { 2, 2, 1, 1, 1, 2, 2 }) == 2;
+		assert majorityElementV2(new int[] { 3, 2, 3 }) == 3;
+		assert majorityElementV2(new int[] { 2, 2, 1, 1, 1, 2, 2 }) == 2;
 	}
 
 	public int majorityElement(int[] nums)
@@ -40,5 +40,33 @@ public class Q169MajorityElement
 		}
 		// 题目中保证了结果必须存在，这里肯定不会走到
 		return -1;
+	}
+
+	/**
+	 * 摩尔投票法
+	 * @param nums
+	 * @return
+	 */
+	public int majorityElementV2(int[] nums)
+	{
+		int count = 1;
+		int maj = nums[0];
+		for (int i = 1; i < nums.length; i++)
+		{
+			if (nums[i] == maj)
+			{
+				count++;
+			}
+			else
+			{
+				count--;
+				if (count == 0)
+				{
+					maj = nums[i];
+					count = 1;
+				}
+			}
+		}
+		return maj;
 	}
 }
