06e569163ac0556ae8f6544c1ebe55bec1fcf153
张云飞 <zhangsmile90@gmail.com>
2020-04-13 17:15:50 +0800

    占坑

--- src/main/java/com/leon/leetcodeleon/q000/q025/Q025ReverseKGroupV2.java
+++ src/main/java/com/leon/leetcodeleon/q000/q025/Q025ReverseKGroupV2.java
@@ -1,117 +1,59 @@
 package com.leon.leetcodeleon.q000.q025;
 
+import com.leon.leetcodeleon.common.ListNode;
+import com.leon.leetcodeleon.util.ListNodeUtils;
+import com.leon.leetcodeleon.util.ObjectUtils;
 import org.junit.Test;
 
-public class Q025ReverseKGroup
+import java.util.ArrayList;
+import java.util.List;
+
+public class Q025ReverseKGroupV2
 {
 
 	@Test
 	public void test()
 	{
-		ListNode l1 = new ListNode(1);
-		ListNode m = l1;
-		m.next = new ListNode(2);
-		m = m.next;
-		m.next = new ListNode(3);
-		m = m.next;
-		m.next = new ListNode(4);
-		m = m.next;
-		m.next = new ListNode(5);
-		m = m.next;
-		m.next = new ListNode(6);
-		//		m = m.next;
-		//		m.next = new ListNode(7);
-		//		m = m.next;
-		//		m.next = new ListNode(8);
-		//		m = m.next;
-		//		m.next = new ListNode(9);
-		//		m = m.next;
-		//		m.next = new ListNode(10);
-		printList(l1);
-
-		ListNode l2 = new ListNode(1);
-		m = l2;
-		m.next = new ListNode(3);
-		m = m.next;
-		m.next = new ListNode(4);
-		printList(l2);
-		//
-		ListNode l3 = new ListNode(2);
-		printList(l3);
-
-		//		printList(reverseKGroup(l1, 2));
-		printList(reverseKGroup(l1, 3));
-		printList(reverseKGroup(l3, 2));
+		ObjectUtils.printListNode(reverseKGroup(ListNodeUtils.generateList(new int[] { 1, 2, 3, 4, 5 }), 2));
+		ObjectUtils.printListNode(reverseKGroup(ListNodeUtils.generateList(new int[] { 1, 2, 3, 4, 5 }), 3));
 	}
 
 	public ListNode reverseKGroup(ListNode head, int k)
 	{
-		if (head == null)
-		{
-			return null;
-		}
-		ListNode tmp = new ListNode(0);
-		tmp.next = head;
-		ListNode first = head;
-		ListNode per = tmp;
-		while (head != null)
-		{
-
-			ListNode end = first;
-			int i = k;
-			while (i > 1 && end !=null &&  end.next != null)
-			{
-				end = end.next;
-				i--;
-			}
-			if (i > 1 || end == null)
-			{
-				break;
-			}
-			ListNode ne = end.next;
-			i = k;
-			while (i > 1)
-			{
-				per.next = first.next;
-				first.next = end.next;
-				end.next = first;
-				first = per.next;
-				i--;
-			}
-
-			first = ne;
-
-			i = k;
-			while (i > 0)
-			{
-				per = per.next;
-				i--;
-			}
-
-		}
-		return tmp.next;
+		// if (head == null)
+		// {
+		// 	return null;
+		// }
+		// List<ListNode> tmp = new ArrayList<>(k);
+		// ListNode pre = new ListNode(0);
+		// pre.next = head;
+		// ListNode p = pre;
+		//
+		// while (head != null)
+		// {
+		// 	tmp = new ArrayList<>(k);
+		// 	int i = k;
+		// 	while (head != null && i > 0)
+		// 	{
+		// 		tmp.add(head);
+		// 		i--;
+		// 		head = head.next;
+		// 	}
+		// 	if (tmp.size() < k)
+		// 	{
+		// 		break;
+		// 	}
+		// 	tmp.get(0).next = tmp.get(k - 1).next;
+		// 	for (i = k - 1; i > 0; i--)
+		// 	{
+		// 		tmp.get(i).next = tmp.get(i);
+		// 	}
+		// 	p.next = tmp.get(k - 1);
+		// 	p = tmp.get(0);
+		// 	head = p.next;
+		// }
+		// return pre.next;
+		return null;
 	}
 
-	private class ListNode
-	{
-		int val;
-		ListNode next;
-
-		ListNode(int x)
-		{
-			val = x;
-		}
-	}
-
-	private void printList(ListNode l)
-	{
-		StringBuilder sb = new StringBuilder();
-		while (l != null)
-		{
-			sb.append(l.val);
-			sb.append("-");
-			l = l.next;
-		}
-		System.out.println(sb);
-	}
 }
---
+++ src/main/java/com/leon/leetcodeleon/q300/q355/Q355Twitter.java
@@ -0,0 +1,10 @@
+package com.leon.leetcodeleon.q300.q355;
+
+import org.junit.Test;
+
+import java.util.List;
+
+public class Q355Twitter
+{
+
+}
