41596333ceca746ce31b98990d507d7416c6fc52
张云飞 <zhangsmile90@gmail.com>
2020-06-09 22:26:37 +0800

    修改

--- src/main/java/com/leon/leetcodeleon/q000/q054/Q054SpiralOrder.java
+++ src/main/java/com/leon/leetcodeleon/q000/q054/Q054SpiralOrder.java
@@ -11,9 +11,10 @@ public class Q054SpiralOrder
 	@Test
 	public void test()
 	{
-		System.out.println(spiralOrder(new int[][] { { 7 }, { 9 }, { 6 } }));
-		System.out.println(spiralOrder(new int[][] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } }));
-		System.out.println(JSONObject.toJSONString(spiralOrder(new int[][] { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 } })));
+		System.out.println(spiralOrder1(new int[][] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } }));
+		// System.out.println(spiralOrder(new int[][] { { 7 }, { 9 }, { 6 } }));
+		// System.out.println(spiralOrder(new int[][] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } }));
+		// System.out.println(JSONObject.toJSONString(spiralOrder(new int[][] { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 } })));
 	}
 
 	public List<Integer> spiralOrder(int[][] matrix)
@@ -43,5 +44,42 @@ public class Q054SpiralOrder
 		}
 		return ans;
 
+	}
+
+	public int[] spiralOrder1(int[][] matrix)
+	{
+		if (matrix.length == 0)
+			return new int[] {};
+		int r1 = 0, r2 = matrix.length - 1;
+		int c1 = 0, c2 = matrix[0].length - 1;
+		int[] res = new int[(r2 + 1) * (c2 + 1)];
+		int k = 0;
+		while (r1 <= r2 && c1 <= c2)
+		{
+			for (int c = c1; c <= c2; c++)
+			{
+				res[k++] = matrix[r1][c];
+			}
+			for (int r = r1 + 1; r <= r2; r++)
+			{
+				res[k++] = matrix[r][c2];
+			}
+			if (r1 < r2 && c1 < c2)
+			{
+				for (int c = c2 - 1; c > c1; c--)
+				{
+					res[k++] = matrix[r2][c];
+				}
+				for (int r = r2; r > r1; r--)
+				{
+					res[k++] = matrix[r][c1];
+				}
+			}
+			r1++;
+			r2--;
+			c1++;
+			c2--;
+		}
+		return res;
 	}
 }
